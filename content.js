c0466c3fa2647a278fe4582409e86236b15a08b9
islu <faneez25@gmail.com>
2020-02-29 20:52:35 +0800

    test: popup.js send message to content.js

--- content.js
+++ content.js
@@ -2,21 +2,20 @@ let thisLocationEggs = document.querySelector('.eggs').children;
 let thisUrl = document.URL;
 let thisLocationIndex = thisUrl[thisUrl.length-1];
 let locations = [null, coast, desert, forest, jungle, alpine, volcano, holiday];
+// controll from popup.js
+let rfBtn = 'rf-on' 
 
-function remove_fog(thisLocationEggs, local)
-{
-	for (let c = 0; c < thisLocationEggs.length; c++)
-	{
+function removeFog(thisLocationEggs, local) {
+	
+	for (let c = 0; c < thisLocationEggs.length; c++) {
 		let description = thisLocationEggs[c].getElementsByTagName('span')[0];
 		let img = thisLocationEggs[c].getElementsByTagName('img')[0];
 		
-		for (let i = 0; i < local.length; i++)
-		{
+		for (let i = 0; i < local.length; i++) {
 			let thisEgg = [];	
 			thisEgg = local[i].split('$');
 			
-			if (thisEgg.includes(description.innerText) == true)
-			{
+			if (thisEgg.includes(description.innerText) == true) {
 				img.src = chrome.extension.getURL('images/'+thisEgg[1]);
 				img.removeAttribute('width');
 				img.removeAttribute('height');
@@ -26,9 +25,23 @@ function remove_fog(thisLocationEggs, local)
 	}
 }
 
-remove_fog(thisLocationEggs, locations[thisLocationIndex]);
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if (request == 'rf-off' || request == 'rf-on') rfBtn = request;
+		
+		console.log(request);
+		//console.log(sender);
+		//console.log(sendResponse);
+	
+	}
+)
 
-console.log('Fog Already Remved! ENJOY!');
+
+if (rfBtn == 'rf-on') {
+	removeFog(thisLocationEggs, locations[thisLocationIndex]);
+	console.log('Fog Already Remved! ENJOY!');
+}	
+
 
 /// new function ///
 
@@ -49,6 +62,8 @@ console.log('Fog Already Remved! ENJOY!');
 
 //// a bug :( /////
 /*
+
+// This egg was buried in a sand dune.        does't show img
 
 let c = ['﻿This drab egg rests far from the water’s edge.$e1.png'];
 let a = c[0].split('$');
--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,11 @@
 
-window.onload = function start()
-{
-	let RemoveFog = document.getElementById("remove-fog");
-	console.log(RemoveFog.checked);
+window.onload = function init() {
+	document.getElementById( 'rf-on' ).onclick = function()  { sendMessage('rf-on'); };
+	document.getElementById( 'rf-off' ).onclick = function() { sendMessage('rf-off'); };
+}
+
+function sendMessage(msg) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, msg);
+	});	
 }
