e7d96e4b442ac3d02ff0d98c1b2cec2d277e8949
islu <faneez25@gmail.com>
2020-03-23 14:33:25 +0800

    new: highlight & show lineage on abandoned egg

--- js/abandoned.js
+++ js/abandoned.js
@@ -2,14 +2,39 @@ let eggs = $(".spr").parent();
 let eggImg;
 
 // show egg name code
-eggs.hover(hoverIn, hoverOut);
+eggs.hover(showCode, hideCode);
+eggs.hover(showLineage, function(){});
+highlight();
+showLineage();
 
-function hoverIn() {
+function showCode() {
   let code = this.href.split('/')[4];
 	eggImg = this.firstChild;
 	this.text = code;	
 }
-function hoverOut() {
+function hideCode() {
 	this.innerHTML = "";
 	this.appendChild(eggImg);
+}
+function highlight() {
+	for (let i = 0; i < eggs.length; i++) {
+		let code = eggs[i].href.split('/')[4];
+		
+		if (code.match(/\d{4,}/g) != null) {
+			eggs[i].style = "background-color: yellow"
+		}
+		if (code.match(/[a-z]{4,}/g) != null) {
+			eggs[i].style = "background-color: yellow"
+		}
+		if (code.match(/[A-Z]{4,}/g) != null) {
+			eggs[i].style = "background-color: yellow"
+		}
+	}
+}
+function showLineage() {
+	//https://dragcave.net/lineage/:code
+	let code = this.href.split('/')[4];
+	$('p').html(function() {
+		return '<iframe src="https://dragcave.net/lineage/'+code+'" width="100%" height="100%" frameborder="0" scrolling="yes""></iframe>'
+	})
 }
