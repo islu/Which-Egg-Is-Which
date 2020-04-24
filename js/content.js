47427e2eb9a588b20f1065126456cd86872e2e6b
islu <faneez25@gmail.com>
2020-04-24 21:03:21 +0800

    new: label rarity on hidden egg, change description background color

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@ let thisUrl = document.URL;
 let thisLocationIndex = thisUrl[thisUrl.length-1];
 let locations = [null, coast, desert, forest, jungle, alpine, volcano, holiday];
 
-function removeFog(thisLocationEggs, local) {
+function remove_fog(thisLocationEggs, local) {
 
 	for (let c = 0; c < thisLocationEggs.length; c++) {
 		let description = thisLocationEggs[c].getElementsByTagName('span')[0];
@@ -16,11 +16,22 @@ function removeFog(thisLocationEggs, local) {
 				img.src = chrome.extension.getURL('images/'+thisEgg[1]);
 				img.removeAttribute('width');
 				img.removeAttribute('height');
+				label_rarity(description, thisEgg[1]);
 				break;
 			}
 		}
 	}
 }
 
-removeFog(thisLocationEggs, locations[thisLocationIndex]);
+function label_rarity(element, key) {
+	if (RARITY[key] != undefined) {
+		switch (RARITY[key][rarity]) {
+			case "Uncommon":    element.style = "background-color:"+SILVER; break;
+			case "Rare":        element.style = "background-color:"+GOLD; break;
+			case "Mythic Rare": element.style = "background-color:"+RED; break;
+		}
+	}
+}
+
+remove_fog(thisLocationEggs, locations[thisLocationIndex]);
 console.log('Fog Already Remved! ENJOY!');
