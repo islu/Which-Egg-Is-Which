f7218e11399a1308a2f02b8bcb0b837631501f47
islu <faneez25@gmail.com>
2021-05-15 08:57:05 +0800

    New: Can automatically click on Easter eggs

--- js/autocollect.js
+++ js/autocollect.js
@@ -2,18 +2,28 @@
 // ( dragoncave.net/trickortreat )
 
 window.onload = function () {
-	getHalloweenCollections();
+	clickHalloweenCollections();
+	clickEasterCollections();
 }
 
 setTimeout(function () {
 	window.location.reload();
 }, 1000 * 60 * 5) // Refresh every 5 minutes
 
-function getHalloweenCollections() {
+function clickHalloweenCollections() {
 	let img = document.getElementsByTagName('img');
 	for (let i = 0; i < img.length; i++) {
 		if (img[i]['alt'] == "Trick, or treat") { 
 			img[i].click();
 		} // Happy Halloween ~ 
+	}
+}
+
+function clickEasterCollections() {
+	let img = document.getElementsByTagName('img');
+	for (let i = 0; i < img.length; i++) {
+		if (img[i]['alt'] == "Festival of Eggs egg") { 
+			img[i].click();
+		}
 	}
 }
