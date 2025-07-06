560fa2225a3532ca5da32d469a65515efd148609
islu <38793955+islu@users.noreply.github.com>
2025-07-06 23:25:21 +0800

    Refactor: project layout (#14)
    * refactor: project layout
    
    * Update src/content/abandoned.js
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/content/collection.js
+++ src/content/collection.js
@@ -1,8 +1,17 @@
-// ※init
+
+// --------- Initialization
+
 setTimeout(() => {AutoCollectCollectibles(5);}, 5000);
 
-// ※functions
+// --------- Functions
+
 // Collect collectibles automatically.
+/**
+ * Automatically finds and clicks on the first collectible image element on the page
+ * and then reloads the page after a specified number of minutes.
+ *
+ * @param {number} [refreshMinute=5] - The number of minutes to wait before reloading the page. Defaults to 5 minutes.
+ */
 function AutoCollectCollectibles(refreshMinute = 5) {
     const c = [...document.getElementsByTagName('img')];
 
